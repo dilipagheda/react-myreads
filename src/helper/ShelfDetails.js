f8e253045e7d3a2095e729d9ecf34cc65a03b51a
Dilip <dilip_agheda@yahoo.com>
2019-10-10 22:33:31 +1100

    Added ShelfDetails helper object to act as enum for shelf id and title to be used across the whole app.

---
+++ src/helper/ShelfDetails.js
@@ -0,0 +1,14 @@
+export const ShelfDetails = {
+	currentlyReading: {
+		title: 'Currently Reading',
+		id: 'currentlyReading'
+	},
+	wantToRead: {
+		title: 'Want to read',
+		id: 'wantToRead'
+	},
+	read: {
+		title: 'Read',
+		id: 'read'
+	}
+};
