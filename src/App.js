dfc3fd74d18dc49d4a212277a87b34a99fedc82e
Dilip <dilip_agheda@yahoo.com>
2019-10-10 22:34:06 +1100

    Added initial state and componentDidMount with API call in App.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import { Route, BrowserRouter as Router } from 'react-router-dom';
 import MainPage from './components/MainPage';
 import SearchPage from './components/SearchPage';
 
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI';
 import './App.css';
 
 class BooksApp extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { books: [] };
+	}
+
+	async componentDidMount() {
+		const books = await BooksAPI.getAll();
+		this.setState({ books });
+		console.log(books);
+	}
+
 	render() {
 		return (
 			<Router>
 				<div className="app">
-					<Route exact path="/" component={MainPage} />
+					<Route exact path="/" render={() => <MainPage books={this.state.books} />} />
 					<Route path="/search" component={SearchPage} />
 				</div>
 			</Router>
