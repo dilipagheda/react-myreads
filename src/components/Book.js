782aea4c44c76e13f6f715b59f565c2d4f8e2d6c
Dilip <dilip_agheda@yahoo.com>
2019-10-11 20:31:09 +1100

    Added props to Book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class Book extends Component {
+	getAuthors = (authors) => {
+		return authors.join(',');
+	};
+
 	render() {
 		return (
 			<div className="book">
@@ -10,8 +14,7 @@ class Book extends Component {
 						style={{
 							width: 128,
 							height: 192,
-							backgroundImage:
-								'url("http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api")'
+							backgroundImage: `url("${this.props.backgroundImage}")`
 						}}
 					/>
 					<div className="book-shelf-changer">
@@ -26,8 +29,8 @@ class Book extends Component {
 						</select>
 					</div>
 				</div>
-				<div className="book-title">The Adventures of Tom Sawyer</div>
-				<div className="book-authors">Mark Twain</div>
+				<div className="book-title">{this.props.title}</div>
+				<div className="book-authors">{this.getAuthors(this.props.authors)}</div>
 			</div>
 		);
 	}
