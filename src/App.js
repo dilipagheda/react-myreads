f5748df2f8166c2c4f79bce9c8901863c511fb9b
Dilip <dilip_agheda@yahoo.com>
2019-10-11 22:29:26 +1100

    Wrote logic for updateShelf function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import MainPage from './components/MainPage';
 import SearchPage from './components/SearchPage';
+import { ShelfDetails } from './helper/ShelfDetails';
 
 import * as BooksAPI from './BooksAPI';
 import './App.css';
@@ -11,6 +12,30 @@ class BooksApp extends React.Component {
 		super(props);
 		this.state = { books: [] };
 	}
+
+	updateShelf = async (bookId, shelf) => {
+		const response = await BooksAPI.update({ id: bookId }, shelf);
+		console.log(response);
+
+		this.setState((prevState) => {
+			let books = prevState.books.map((book) => {
+				if (response.currentlyReading.includes(book.id)) {
+					book.shelf = ShelfDetails.currentlyReading.id;
+				} else if (response.read.includes(book.id)) {
+					book.shelf = ShelfDetails.read.id;
+				} else if (response.wantToRead.includes(book.id)) {
+					book.shelf = ShelfDetails.wantToRead.id;
+				} else {
+					book.shelf = ShelfDetails.none.id;
+				}
+				return book;
+			});
+			books = books.filter((book) => book.shelf !== ShelfDetails.none.id);
+			return {
+				books
+			};
+		});
+	};
 
 	async componentDidMount() {
 		const books = await BooksAPI.getAll();
@@ -22,7 +47,11 @@ class BooksApp extends React.Component {
 		return (
 			<Router>
 				<div className="app">
-					<Route exact path="/" render={() => <MainPage books={this.state.books} />} />
+					<Route
+						exact
+						path="/"
+						render={() => <MainPage books={this.state.books} updateShelf={this.updateShelf} />}
+					/>
 					<Route path="/search" component={SearchPage} />
 				</div>
 			</Router>
