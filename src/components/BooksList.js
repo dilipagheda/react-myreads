261689b79d10ab0f4fa3bdd5b0d6be31cda9e388
Dilip <dilip_agheda@yahoo.com>
2019-10-11 20:32:58 +1100

    Added a method to render books via props in BooksList component

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -2,20 +2,21 @@ import React, { Component } from 'react';
 import Book from './Book';
 
 class BooksList extends Component {
+	renderBooks = () => {
+		return this.props.books.map((book) => (
+			<li>
+				<Book
+					key={book.id}
+					backgroundImage={book.imageLinks.thumbnail}
+					title={book.title}
+					authors={book.authors}
+				/>
+			</li>
+		));
+	};
+
 	render() {
-		return (
-			<ol className="books-grid">
-				<li>
-					<Book />
-				</li>
-				<li>
-					<Book />
-				</li>
-				<li>
-					<Book />
-				</li>
-			</ol>
-		);
+		return <ol className="books-grid">{this.renderBooks()}</ol>;
 	}
 }
 
