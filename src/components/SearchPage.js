03e8060cc557bf1badfb58c3710acf10fa067829
Dilip <dilip_agheda@yahoo.com>
2019-10-12 19:59:23 +1100

    Completed implementation of SearchPage component

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import BooksList from './BooksList';
+import * as BooksAPI from '../BooksAPI';
 
 class SearchPage extends Component {
+	state = { result: [] };
+
+	resetState = () => {
+		this.setState({ result: [] });
+	};
+
+	handleChange = async (event) => {
+		const searchTerm = event.target.value;
+		if (searchTerm.length === 0) {
+			this.resetState();
+			return;
+		}
+		let result = await BooksAPI.search(searchTerm);
+		if (result.error && result.error === 'empty query') {
+			this.resetState();
+			return;
+		}
+		result = result.filter((book) => book.authors !== undefined && book.imageLinks !== undefined);
+
+		this.setState({ result });
+	};
 	render() {
 		return (
 			<div className="search-books">
@@ -10,11 +33,15 @@ class SearchPage extends Component {
 						Close
 					</button>
 					<div className="search-books-input-wrapper">
-						<input type="text" placeholder="Search by title or author" />
+						<input type="text" placeholder="Search by title or author" onChange={this.handleChange} />
 					</div>
 				</div>
 				<div className="search-books-results">
-					<ol className="books-grid" />
+					<BooksList
+						result={this.state.result}
+						books={this.props.books}
+						updateShelf={this.props.updateShelf}
+					/>
 				</div>
 			</div>
 		);
