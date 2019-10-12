b39e229cbf018d9bf0c1d1e68318a68dc595aac5
Dilip <dilip_agheda@yahoo.com>
2019-10-12 19:59:55 +1100

    Completed implementation of BookList component

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
 import Book from './Book';
+import { ShelfDetails } from '../helper/ShelfDetails';
 
 class BooksList extends Component {
 	renderBooks = () => {
-		return this.props.books.map((book) => (
-			<li>
-				<Book
-					key={book.id}
-					bookId={book.id}
-					shelf={book.shelf}
-					backgroundImage={book.imageLinks.thumbnail}
-					title={book.title}
-					authors={book.authors}
-					updateShelf={this.props.updateShelf}
-				/>
-			</li>
-		));
+		return this.props.books.map((book) => {
+			return (
+				<li>
+					<Book key={book.id} book={book} updateShelf={this.props.updateShelf} />
+				</li>
+			);
+		});
+	};
+
+	renderSearchedBooks = () => {
+		return this.props.result.map((searchedBook) => {
+			const bookOnShelf = this.props.books.find((book) => book.id === searchedBook.id);
+			if (bookOnShelf) {
+				searchedBook.shelf = bookOnShelf.shelf;
+			} else {
+				searchedBook.shelf = ShelfDetails.none.id;
+			}
+
+			return (
+				<li>
+					<Book key={searchedBook.id} book={searchedBook} updateShelf={this.props.updateShelf} />
+				</li>
+			);
+		});
 	};
 
 	render() {
-		return <ol className="books-grid">{this.renderBooks()}</ol>;
+		return (
+			<ol className="books-grid">
+				{
+					this.props.result ? this.renderSearchedBooks() :
+					this.renderBooks()}
+			</ol>
+		);
 	}
 }
 
